58e54b3da667e2bcb9a73943089b7710e0bf38bd
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2025-03-28 22:50:55 +0100

    porteus-net-install.sh, TODO: version changed in v0.3.3
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- porteus-net-install.sh
+++ porteus-net-install.sh
@@ -5,11 +5,11 @@
 # Support for expert users wishing to test on-demand installation starts from
 #
 # URL: https://raw.githubusercontent.com/robang74/porteus-usb-installer
-# VER:                         /refs/tags/v0.3.2/porteus-net-install.sh
+# VER:                         /refs/tags/v0.3.3/porteus-net-install.sh
 # MAN:                           ?tab=readme-ov-file#usage-quick--dirty
 #
 # It is suggest testing on a spare machine that does NOT hold valuable data,
-# possibly using a Porteus MATE v0.3.2 with the network support available.
+# possibly using a Porteus MATE v5.1 with the network support available.
 #
 ################################################################################
 set +o noclobber
@@ -95,7 +95,7 @@ proj="porteus-usb-installer"
 repo="https://github.com/$user/$proj"
 rawu="https://raw.githubusercontent.com/$user/$proj"
 
-tagver="v0.3.2"
+tagver="v0.3.3"
 reftyp="tags"
 
 ################################################################################
@@ -116,7 +116,7 @@ echo me cmd:$$:$cmd; bash -i <(cat test.sh)
 # 0:/dev/fd/62:2170760 ppid:2170724 pcmd:bash /dev/fd/63
 #                      stat:2170724 (bash) S 1786462 2170724 17
 
-tagver="v0.3.2" # To replace with the lastest available in tags
+tagver="v0.3.3" # To replace with the lastest available in tags
 rawurl="https://raw.githubusercontent.com/robang74"
 rawurl="$rawurl/porteus-usb-installer/refs/tags/$tagver"
 rawurl="$rawurl/porteus-net-install.sh"
