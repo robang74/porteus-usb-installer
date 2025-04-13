7feb842306ebcbd50dbad1bd9734e319bf8773bc
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2025-04-13 07:46:17 +0200

    tools/porteux-usb-install.sh: added but working in progress, p.2
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- tools/porteux-usb-install.sh
+++ tools/porteux-usb-install.sh
@@ -7,21 +7,49 @@ set +o noclobber
 set +u
 set -e
 
-usage_strn="/path/porteux-bla-bla.iso [/dev/]sdx [it]"
+wdr=$(dirname "$0")
+shs=$(basename "$0")
+
+variant="porteux"
+store_dirn="moonwalker"
+usage_strn="/path/$variant-bla-bla.iso [/dev/]sdx [it]"
+
+export DEVEL=${DEVEL:-0}
+
+function perr() { { printf "$@"; } >&2; }
+function errexit() { echo; exit ${1:-1}; }
 
 function usage() {
     printf \\n"USAGE: bash ${shs:-$(basename $0)} $usage_strn"\\n
     eval "$@"
 }
 
+function search() {
+    local d ldirs=". $wdr $workingd_path .." f="${1:-}"
+    test -n "$f" || return 1
+    test "$(basename $wdr)"  == "tmp" && ldirs="$ldirs .."
+    for d in $ldirs; do
+        if [ -d "$d" -a -r $d/$f ]; then echo "$d/$f"; return 0; fi
+    done; return 1
+}
+
+function missing() {
+    perr \\n"ERROR: file '${1:-}' is missing or wrong type, abort!"\\n
+    errexit
+}
+
 script=$(search porteus-usb-install.sh ||:)
-isofle=${1:-}
+isofile=${1:-}
 dev=${2:-1}
 
-test -r $isofile || missing $isofile
-test -r $script || missing $script
-test -b $dev || missing $dev
+test -b "$dev" || dev="/dev/$dev"
+test -b "$dev" || missing "$dev"
+test -r "$script" || missing "$script"
+test -r "$isofile" || missing "$isofile"
 
-echo "$isofle" | grep -qi "porteux" || usage exit 1
+if ! echo "$isofile" | grep -qi "$variant"; then    
+    usage errexit
+    perr "ERROR: this script requires a '$variant' ISO image file"\\n
+fi
 
-DEVEL=${DEVEL:-0} VARIANT=porteux bash $script $isofle $dev ${3:-} --ext4-install
+DEVEL=$DEVEL VARIANT=$variant bash $script "$isofile" $dev ${3:-} --ext4-install
